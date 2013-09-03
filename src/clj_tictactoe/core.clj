1c1932c82af117a609b7c41c8807de14d353c1e7
Timothy Hahn <timyhahn@gmail.com>
2013-09-03 19:12:43 -0400

    Added board state changer and free/player helpers
    * Added apply-move which returns a modified board
    * Added free? and any-free? to help validate moves on a board
    * Added other-player to flip x and o

--- src/clj_tictactoe/core.clj
+++ src/clj_tictactoe/core.clj
@@ -2,13 +2,15 @@
   (:gen-class))
 
 (defn make-board []
+  "Returns an empty board"
   (vec (repeat 9 " ")))
 
 (defn limit-str []
+  "Returns the string used to separate rows"
   (apply str(vec (repeat 5 "-"))))
 
-;; Prints out the row with proper spacing/delimiting
 (defn row-str [row]
+  "Returns the string of the row with the proper spacing/delimiting"
   (let [rows (atom [])]
     (loop [n 0]
       (when (< n 3)
@@ -19,6 +21,8 @@
     (apply str @rows)))
 
 (defn print-board [board]
+  "Prints the board in it's current state.
+  SIDE EFFECTS: Prints to console"
   (do
     (loop [n 0]
       (when (< n 3)
@@ -27,16 +31,56 @@
           (println (limit-str)))
         (recur (inc n))))))
   
+(defn is-winner? [board player]
+  false)
 
 (defn winner-of [board]
+  "Returns either the character of the winner or nil if no winner"
   nil)
+
+(defn free? [board n]
+  "Returns whether or not the member on the board is equal to an empty character"
+  (= (nth board n) " "))
+
+(defn any-free? [board]
+  "True if there is a free spot on the board, otherwise nil"
+  (some #(= " " %) board))
+
+(defn apply-move [board n player]
+  "Returns a board with the move performed if possible"
+  (if (free? board n)
+      (assoc board n player)
+      board))
+
+(defn other-player [player]
+  (if (= player "x")
+    "o"
+    "x"))
 
 (defn -main
   "Main class"
   [& args]
   (println "Welcome to Clojure Tic Tac Toe")
-  (let [board (atom(make-board))]
-    (while (= (winner-of board) nil)
+  (let [board (atom (make-board)) player (atom "x")]
+    (while (and
+             (any-free? @board)
+             (= (winner-of @board) nil))
+      (print-board @board)
+      (println "Player" @player)
       (println "Input Move(1-9):")
-      (let [move (read-line)]
-        (println move)))))
+      (try 
+        (let [move (dec(Integer/parseInt (read-line)))]
+          (if (and
+            (>= move 0)
+            (<= move 8)
+            (free? @board move))
+              (do (swap! board apply-move move @player)
+               (swap! player other-player))
+              (println "Please input a valid move")))
+        (catch NumberFormatException nfe (println "Please use a number to indicate your move"))))
+ 
+      (print-board @board)
+      (let [winner (winner-of board)]
+        (if winner
+          (println "Congrats to winner" winner)
+          (println "No one wins")))))
--- test/clj_tictactoe/core_test.clj
+++ test/clj_tictactoe/core_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer :all]
             [clj-tictactoe.core :refer :all]))
 
+(def empty-board
+  (make-board))
+
 (def empty-board-str 
 " | | 
 -----
@@ -21,14 +24,43 @@
 o|o|x
 ")
 
+(def full-board
+  ["x" "o" "x" "o" "x" "o" "x" "o" "x"])
 
-(deftest boardprint
+(deftest print-board-test
   (testing "The board prints properly"
-    (is (= (with-out-str(print-board (make-board))) (str empty-board-str)))
+    (is (= (with-out-str(print-board empty-board)) (str empty-board-str)))
     (is (= (with-out-str(print-board test-board)) (str test-board-str)))))
 
-(deftest winner
+(deftest winner-test
+  (testing "Whether or not we can tell whether a player is a winner"
+    (is (= (is-winner? empty-board "x") false))
+    (is (= (is-winner? empty-board "o") false)))
+
   (testing "The winner is properly returned"
-    (is (= (winner-of (make-board)) nil))))
+    (is (= (winner-of empty-board) nil))))
+
+(deftest apply-move-test
+  (testing "Whether applying a move works"
+    (is (= (apply-move empty-board 0 "x") ["x" " " " " " " " " " " " " " " " "]))
+    (is (= (apply-move test-board 0 "x") test-board))
+    (is (= (apply-move test-board 3 "x") ["x" "x" "o" "x" "x" " " "o" "o" "x"]))
+    (is (= (apply-move full-board 0 "x") full-board))))
+
+(deftest freeness-test
+  (testing "If specific cells are free"
+    (is (= (free? empty-board 0) true))
+    (is (= (free? test-board 0) false))
+    (is (= (free? test-board 3) true))
+    (is (= (free? full-board 0) false)))
+
+  (testing "If any cells are free"
+    (is (= (any-free? empty-board) true))
+    (is (= (any-free? test-board) true))
+    (is (= (any-free? full-board) nil))))
     
+(deftest other-player-test
+  (testing "The other player is returned correctly"
+    (is (= (other-player "x") "o"))
+    (is (= (other-player "o") "x"))))
 
